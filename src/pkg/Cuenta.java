55c4236773e38906eb5def5c1ecff8fb86bd9a48
usuario <usuario@pc20-018>
2023-10-10 10:52:35 +0200

    Paso 1, cread TDD

---
+++ src/pkg/Cuenta.java
@@ -0,0 +1,33 @@
+package pkg;
+
+public class Cuenta {
+
+	double saldo;
+
+	public Cuenta(String numero, String titular, double saldo) {
+		super();
+		this.saldo = saldo;
+	}
+	
+	public double getSaldo() {
+		return 1000;
+	}
+
+	public void setSaldo(double saldo) {
+		this.saldo = saldo;
+	}
+
+	public void ingresar(int i) {
+		
+		
+	}
+
+	public static Integer retirar(int i) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	
+	
+	
+}
